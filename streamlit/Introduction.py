eb8946a25e8796de0ffd6a23b74e03d5f839096e
clem-goust <clement.goustat@gmail.com>
2024-10-16 17:31:23 +0200

    Sligthly amended Introduction

--- streamlit/Introduction.py
+++ streamlit/Introduction.py
@@ -7,11 +7,13 @@ st.image("streamlit/images/garanteo.png")
 
 st.title('Project Garanteo')
 
-st.write('''Welcome to this Streamlit. These pages summarize my work on project Garanteo.\n
-    Garanteo is a fictional insurance company running 100% online.\n
-    As such, they need input on :\n
-    - Their users, leads, clients\n
-    - Which leads should be prioritized by their sales team\n 
-    - How well their marketing campaigns have done so far\n
-         
-Project Garanteo is the final assignment of my 340h Data Analyst training with Databird.''')
+st.markdown("**Welcome to this Streamlit. These pages summarize my work on project Garanteo.**")
+with st.container(border=True):
+    st.write('''
+        *Garanteo is a fictional insurance company running 100% online.*\n
+        *As such, they need input on :*\n
+        - *Their users, leads, clients*\n
+        - *Which leads should be prioritized by their sales team*\n 
+        - *How well their marketing campaigns have done so far*\n
+    ''')         
+st.write("**Project Garanteo is the final assignment of my 340h Data Analyst training with Databird.**")
